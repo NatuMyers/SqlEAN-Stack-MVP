fb29a515a4b7e313ec505346da1ebee5b8accff7
Natu Myers <me@natumyers.com>
2016-07-21 09:13:55 -0400

    dashboard experiment

--- bower_components/angular-post-fix/postfix.js
+++ bower_components/angular-post-fix/postfix.js
@@ -17,44 +17,47 @@ angular.module('httpPostFix', [], function($httpProvider)
          */
         var param = function(obj)
         {
-            var query = '';
-            var name, value, fullSubName, subName, subValue, innerObj, i;
+          // Use x-www-form-urlencoded Content-Type
+          $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
 
-            for(name in obj)
-            {
-                value = obj[name];
+          /**
+           * The workhorse; converts an object to x-www-form-urlencoded serialization.
+           * @param {Object} obj
+           * @return {String}
+           */
+          var param = function(obj) {
+            var query = '', name, value, fullSubName, subName, subValue, innerObj, i;
 
-                if(value instanceof Array)
-                {
-                    for(i=0; i<value.length; ++i)
-                    {
-                        subValue = value[i];
-                        fullSubName = name + '[' + i + ']';
-                        innerObj = {};
-                        innerObj[fullSubName] = subValue;
-                        query += param(innerObj) + '&';
-                    }
+            for(name in obj) {
+              value = obj[name];
+
+              if(value instanceof Array) {
+                for(i=0; i<value.length; ++i) {
+                  subValue = value[i];
+                  fullSubName = name + '[' + i + ']';
+                  innerObj = {};
+                  innerObj[fullSubName] = subValue;
+                  query += param(innerObj) + '&';
                 }
-                else if(value instanceof Object)
-                {
-                    for(subName in value)
-                    {
-                        subValue = value[subName];
-                        fullSubName = name + '[' + subName + ']';
-                        innerObj = {};
-                        innerObj[fullSubName] = subValue;
-                        query += param(innerObj) + '&';
-                    }
+              }
+              else if(value instanceof Object) {
+                for(subName in value) {
+                  subValue = value[subName];
+                  fullSubName = name + '[' + subName + ']';
+                  innerObj = {};
+                  innerObj[fullSubName] = subValue;
+                  query += param(innerObj) + '&';
                 }
-                else if(value !== undefined && value !== null)
-                {
-                    query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
-                }
+              }
+              else if(value !== undefined && value !== null)
+                query += encodeURIComponent(name) + '=' + encodeURIComponent(value) + '&';
             }
 
             return query.length ? query.substr(0, query.length - 1) : query;
-        };
+          };
 
-        return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
-    }];
+          // Override $http service's default transformRequest
+          $httpProvider.defaults.transformRequest = [function(data) {
+            return angular.isObject(data) && String(data) !== '[object File]' ? param(data) : data;
+          }];
 });
